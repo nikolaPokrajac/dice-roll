fcca1c11be973bbbee9327ad4ab8e003aabe78df
nikolaPokrajac <nikolapokrajac92@gmail.com>
2019-08-11 12:35:37 +0200

    Updates

---
+++ precache-manifest.b5eca36fec69f3bf622d61d7f89313b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81dccef8227c6210f5f2d1b4df21a372",
+    "url": "/dice-roll/index.html"
+  },
+  {
+    "revision": "0dc9beb160032b67fb3d",
+    "url": "/dice-roll/static/css/main.f4f5a7f3.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/dice-roll/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "0dc9beb160032b67fb3d",
+    "url": "/dice-roll/static/js/main.134662c3.chunk.js"
+  },
+  {
+    "revision": "c5e700a23ffa76a3a62a",
+    "url": "/dice-roll/static/js/runtime~main.bb3442c3.js"
+  }
+]);
